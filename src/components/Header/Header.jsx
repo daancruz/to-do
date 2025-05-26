5389f12e8024f41e96940d1830aabe7c76ec0fa6
Danilo <daan@MacBook-de-danilo.local>
2025-05-26 20:38:34 -0300

    atualização no endereço das imagens

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,8 @@
 const Header = () => {
   return (
     <header>
-        <img src="/assets/img/2-do-logo.png" alt="Logo do to-do" />
-        <h1>2-do</h1>
+        <img src="./assets/img/2-do-logo.png" alt="Logo do to-do" />
+        <h1>2-ado</h1>
     </header>
   )
 }
