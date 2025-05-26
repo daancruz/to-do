d75709314392670894b7bb10c03216af1f5b1624
Danilo <daan@MacBook-de-danilo.local>
2025-05-26 20:42:37 -0300

    ajuste no titulo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,7 @@ const Header = () => {
   return (
     <header>
         <img src="./assets/img/2-do-logo.png" alt="Logo do to-do" />
-        <h1>2-ado</h1>
+        <h1>2-do</h1>
     </header>
   )
 }
